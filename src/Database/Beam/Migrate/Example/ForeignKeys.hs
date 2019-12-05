ef723da7a8f7cf29c9cb4473336074ea200748fb
Alfredo Di Napoli <alfredo@well-typed.com>
2019-12-05 17:03:11 +0100

    Restructure README and fix small bug

--- src/Database/Beam/Migrate/Example/ForeignKeys.hs
+++ src/Database/Beam/Migrate/Example/ForeignKeys.hs
@@ -3,32 +3,39 @@
 {-# LANGUAGE TypeFamilies         #-}
 module Database.Beam.Migrate.Example.ForeignKeys where
 
-import           Data.Text                      ( Text )
+import           Data.Text                                ( Text )
 
 import           GHC.Generics
 
+import           Control.Exception                        ( bracket )
+
 import           Database.Beam.Postgres
-import           Database.Beam.Schema           ( Beamable
-                                                , Columnar
-                                                , Database
-                                                , PrimaryKey
-                                                , TableEntity
-                                                , defaultDbSettings
-                                                )
-import qualified Database.Beam.Schema          as Beam
-import           Database.Beam.Schema.Tables    ( primaryKey )
+import           Database.Beam.Schema                     ( Beamable
+                                                          , Columnar
+                                                          , Database
+                                                          , PrimaryKey
+                                                          , TableEntity
+                                                          , defaultDbSettings
+                                                          )
+import qualified Database.Beam.Schema                    as Beam
+import           Database.Beam.Schema.Tables              ( primaryKey )
 
-import           Database.Beam.Migrate          ( Schema
-                                                , fromAnnotatedDbSettings
-                                                , defaultAnnotatedDbSettings
-                                                , AnnotatedDatabaseSettings
-                                                )
+import           Database.Beam.Migrate                    ( Schema
+                                                          , fromAnnotatedDbSettings
+                                                          , defaultAnnotatedDbSettings
+                                                          , AnnotatedDatabaseSettings
+                                                          , Migration
+                                                          , printMigration
+                                                          , migrate
+                                                          , runMigration
+                                                          )
 
+import qualified Database.PostgreSQL.Simple              as Pg
+import           Database.Beam.Postgres                   ( runBeamPostgres )
 
 --
 -- Example
 --
-
 
 data CitiesT f = Flower
   { ctCity     :: Columnar f Text
@@ -60,9 +67,23 @@ instance Beam.Table WeatherT where
     deriving (Generic, Beamable)
   primaryKey = WeatherID . wtId
 
--- Modify the field names to be compliant with William Yao's format.
+-- This are 'AnnotatedDatabaseSettings' derived directly from "stock" 'DatabaseSettings'.
 forecastDB :: AnnotatedDatabaseSettings Postgres ForecastDB
 forecastDB = defaultAnnotatedDbSettings defaultDbSettings
 
 hsSchema :: Schema
 hsSchema = fromAnnotatedDbSettings forecastDB
+
+exampleShowMigration :: IO ()
+exampleShowMigration = withBeamTestDb printMigration
+
+withBeamTestDb :: (Migration Pg -> Pg ()) -> IO ()
+withBeamTestDb action = do
+  let connInfo = "host=localhost port=5432 dbname=beam-test-forecast-db"
+  bracket (Pg.connectPostgreSQL connInfo) Pg.close $ \conn ->
+    Pg.withTransaction conn $ runBeamPostgres conn $ do
+      let mig = migrate conn hsSchema
+      action mig
+
+exampleAutoMigration :: IO ()
+exampleAutoMigration = withBeamTestDb runMigration
