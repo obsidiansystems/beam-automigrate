8d69efaf884f10282d898be34322514af56ad8f4
Ali Abrar <aliabrar@gmail.com>
2020-12-07 19:46:43 -0500

    Check whether identifier is a reserved keyword when deciding whether to escape

--- src/Database/Beam/AutoMigrate/Util.hs
+++ src/Database/Beam/AutoMigrate/Util.hs
@@ -118,12 +118,97 @@ sqlEscaped t = if sqlValidUnescaped t
 sqlValidUnescaped :: Text -> Bool
 sqlValidUnescaped t = case T.uncons t of
   Nothing -> True
-  Just (c, rest) -> validUnescapedHead c && validUnescapedTail rest
+  Just (c, rest) -> validUnescapedHead c && validUnescapedTail rest && not (sqlIsReservedKeyword t)
   where
     validUnescapedHead c = c `elem` ("1234567890_"::String) || isAlpha c
     validUnescapedTail = all
       (\r -> (isAlpha r && isLower r) || r `elem` ("1234567890$_"::String)) . T.unpack
 
+sqlIsReservedKeyword :: Text -> Bool
+sqlIsReservedKeyword t = T.toCaseFold t `elem` postgresKeywordsReserved
+
+-- | Reserved keywords according to
+-- https://www.postgresql.org/docs/current/sql-keywords-appendix.html
+postgresKeywordsReserved :: [Text]
+postgresKeywordsReserved = map T.toCaseFold
+  [ "ALL"
+  , "ANALYSE"
+  , "ANALYZE"
+  , "AND"
+  , "ANY"
+  , "ARRAY"
+  , "AS"
+  , "ASC"
+  , "ASYMMETRIC"
+  , "BOTH"
+  , "CASE"
+  , "CAST"
+  , "CHECK"
+  , "COLLATE"
+  , "COLUMN"
+  , "CONSTRAINT"
+  , "CREATE"
+  , "CURRENT_CATALOG"
+  , "CURRENT_DATE"
+  , "CURRENT_ROLE"
+  , "CURRENT_TIME"
+  , "CURRENT_TIMESTAMP"
+  , "CURRENT_USER"
+  , "DEFAULT"
+  , "DEFERRABLE"
+  , "DESC"
+  , "DISTINCT"
+  , "DO"
+  , "ELSE"
+  , "END"
+  , "EXCEPT"
+  , "FALSE"
+  , "FETCH"
+  , "FOR"
+  , "FOREIGN"
+  , "FROM"
+  , "GRANT"
+  , "GROUP"
+  , "HAVING"
+  , "IN"
+  , "INITIALLY"
+  , "INTERSECT"
+  , "INTO"
+  , "LATERAL"
+  , "LEADING"
+  , "LIMIT"
+  , "LOCALTIME"
+  , "LOCALTIMESTAMP"
+  , "NOT"
+  , "NULL"
+  , "OFFSET"
+  , "ON"
+  , "ONLY"
+  , "OR"
+  , "ORDER"
+  , "PLACING"
+  , "PRIMARY"
+  , "REFERENCES"
+  , "RETURNING"
+  , "SELECT"
+  , "SESSION_USER"
+  , "SOME"
+  , "SYMMETRIC"
+  , "TABLE"
+  , "THEN"
+  , "TO"
+  , "TRAILING"
+  , "TRUE"
+  , "UNION"
+  , "UNIQUE"
+  , "USER"
+  , "USING"
+  , "VARIADIC"
+  , "WHEN"
+  , "WHERE"
+  , "WINDOW"
+  , "WITH"
+  ]
 
 sqlSingleQuoted :: Text -> Text
 sqlSingleQuoted t = "'" <> t <> "'"
