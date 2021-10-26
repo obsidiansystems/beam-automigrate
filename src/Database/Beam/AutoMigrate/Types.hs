714bd2f7016bdf5ed780939cf5049df337b8d1b0
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2021-10-26 21:26:37 +0100

    Add IsString TableName instance

--- src/Database/Beam/AutoMigrate/Types.hs
+++ src/Database/Beam/AutoMigrate/Types.hs
@@ -101,6 +101,9 @@ newtype TableName = TableName
   }
   deriving (Show, Eq, Ord, NFData, Generic)
 
+instance IsString TableName where
+  fromString = TableName . T.pack
+
 data Table = Table
   { tableConstraints :: Set TableConstraint,
     tableColumns :: Columns
